a3aaa8217321bd24446e8f8805059a13fe1796ff
tsukasa nishioka <id6tztx7912@gmail.com>
2026-01-17 19:42:46 +0900

    Initial Commit

---
+++ tub-cleaning/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tub-cleaning
+//
+//  Created by 西岡宰 on 2026/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tub-cleaning/tub_cleaningApp.swift
@@ -0,0 +1,17 @@
+//
+//  tub_cleaningApp.swift
+//  tub-cleaning
+//
+//  Created by 西岡宰 on 2026/01/17.
+//
+
+import SwiftUI
+
+@main
+struct tub_cleaningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
